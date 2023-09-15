34e6cbe31c5731efa729029faa4dc28a8d6b6c1d
DavidABowman <2davidbowman@gmail.com>
2023-09-15 17:49:33 +0100

    Create LCE13.py

---
+++ LCE/LCE13.py
@@ -0,0 +1,23 @@
+#Given a roman numeral, convert it to an integer.
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        map = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        } #Got This Part On Own, However I Put Integers in Quotes
+ 
+        ans = 0
+
+        for i in range(len(s)):
+            if i < len(s) - 1 and map[s[i]] < map[s[i+1]]:
+                ans -= map[s[i]]
+            else:
+                ans += map[s[i]]
+                
+        return ans
