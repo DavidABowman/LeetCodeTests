52643e88d05b663ca92f9651fa6840cace08378f
DavidABowman <2davidbowman@gmail.com>
2023-10-05 22:07:19 +0100

    Create LCE110.py

---
+++ LCE/LCE110.py
@@ -0,0 +1,20 @@
+#Given a binary tree, determine if it is height-balanced
+#A height-balanced binary tree is a binary tree in which the depth 
+#of the two subtrees of every node never differs by more than one.
+
+.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return (self.Height(root) >= 0)
+    def Height(self, root: Optional[TreeNode]) -> bool:
+        if root is None:  return 0
+        leftHeight, rightHeight = self.Height(root.left), self.Height(root.right)
+        if leftHeight < 0 or rightHeight < 0 or abs(leftHeight - rightHeight) > 1:  return -1
+        return max(leftHeight, rightHeight) + 1
