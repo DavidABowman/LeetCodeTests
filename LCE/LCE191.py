d6e9658dcee3d40dc27e45dad9b0505b6763f2e8
DavidABowman <2davidbowman@gmail.com>
2023-10-18 22:06:19 +0100

    Create LCE191.py

---
+++ LCE/LCE191.py
@@ -0,0 +1,8 @@
+#Write a function that takes the binary representation of an unsigned integer 
+#and returns the number of '1' bits it has (also known as the Hamming weight).
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return bin(n)[2:].count('1')        
+    #Count total 1 in number
+    #Return total value
