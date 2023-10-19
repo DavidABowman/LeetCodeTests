242073558354fef377342897eddaeda674ccaa25
DavidABowman <2davidbowman@gmail.com>
2023-10-19 21:09:43 +0100

    Create LCE202.py

---
+++ LCE/LCE202.py
@@ -0,0 +1,13 @@
+#A happy number is a number defined by the following process:
+
+#Starting with any positive integer, replace the number by the sum of the squares of its digits.
+#Repeat the process until the number equals 1 (where it will stay), 
+#or it loops endlessly in a cycle which does not include 1.
+#Those numbers for which this process ends in 1 are happy.
+
+#W will return to this 
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        return  False if  n in [2,3,4,5,6,8,9] else True if n == 1 else self.isHappy(sum(pow(int(digit),2) for digit in str(n)))
+        
