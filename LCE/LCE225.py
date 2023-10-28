3c3182fd7f7e730c20133a4441091ca2b1196ef7
DavidABowman <2davidbowman@gmail.com>
2023-10-28 21:03:18 +0100

    Create LCE225.py

---
+++ LCE/LCE225.py
@@ -0,0 +1,31 @@
+#Implement a last-in-first-out (LIFO) stack using only two queues. 
+#The implemented stack should support all the functions of a normal stack 
+#(push, top, pop, and empty)
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+        
+#O(N) time and space complexity
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
