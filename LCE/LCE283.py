a93f4b4ea9e5911896094576907abf792c1aeefd
DavidABowman <2davidbowman@gmail.com>
2023-11-10 21:04:27 +0000

    Create LCE283.py

---
+++ LCE/LCE283.py
@@ -0,0 +1,16 @@
+#Given an integer array nums, 
+#move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+
+            if nums[slow] != 0:
+                slow += 1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
