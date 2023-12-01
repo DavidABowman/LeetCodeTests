af4aa6524d67305a246f73a4e6900284978f528c
DavidABowman <2davidbowman@gmail.com>
2023-12-01 21:01:33 +0000

    Create LCE409.py

---
+++ LCE/LCE409.py
@@ -0,0 +1,18 @@
+#Given a string s which consists of lowercase or uppercase letters, 
+#return the length of the longest palindrome that can be built with those letters.
+
+    def longestPalindrome(self, s: str) -> int:
+        odd_count = 0
+        d = {}
+        for ch in s:
+            if ch in d:
+                d[ch] += 1
+            else:
+                d[ch] = 1
+            if d[ch] % 2 == 1:
+                odd_count += 1
+            else:
+                odd_count -= 1
+        if odd_count > 1:
+            return len(s) - odd_count + 1
+        return len(s)
