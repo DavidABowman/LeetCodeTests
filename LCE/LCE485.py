1c39b442a1995a4453aef5483211fd3ec06e9b4a
DavidABowman <2davidbowman@gmail.com>
2023-12-15 20:26:56 +0000

    Create LCE485.py

---
+++ LCE/LCE485.py
@@ -0,0 +1,14 @@
+#Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        max_ones = 0
+        current_ones = 0
+
+        for num in nums:
+            if num == 1:
+                current_ones += 1
+                max_ones = max(max_ones, current_ones)
+            else:
+                current_ones = 0
+        return max_ones
